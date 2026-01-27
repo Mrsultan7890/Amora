28caf9a43f0816920a91e90083e63bc8f27f0ddd
Mrsultan7890 <khansalahuddin2025@gmail.com>
2026-01-27 19:40:22 +0000

    siusidkd

--- frontend/lib/features/profile/presentation/pages/amoradev_chat_page.dart
+++ frontend/lib/features/profile/presentation/pages/amoradev_chat_page.dart
@@ -238,8 +238,14 @@ class _AmoraDevChatPageState extends State<AmoraDevChatPage> {
         final qrCode = qrValidationResult.qrCode!;
         final painter = QrPainter.withQr(
           qr: qrCode,
-          color: const Color(0xFF000000),
-          emptyColor: const Color(0xFFFFFFFF),
+          eyeStyle: const QrEyeStyle(
+            eyeShape: QrEyeShape.square,
+            color: Color(0xFF000000),
+          ),
+          dataModuleStyle: const QrDataModuleStyle(
+            dataModuleShape: QrDataModuleShape.square,
+            color: Color(0xFF000000),
+          ),
           gapless: false,
         );
 
